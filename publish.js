c2917d711bba94ed5f734894fe05e3769c2dd3f6
Konstantin Gladkikh <birhoff@gmail.com>
2016-02-22 21:48:25 +0300

    Fix

--- publish.js
+++ publish.js
@@ -2,8 +2,7 @@ var argv = require('minimist')(process.argv.slice(2));
 
 var token = process.env.GH_TOKEN,
     repoSlug = process.env.TRAVIS_REPO_SLUG,
-    number = process.env.TRAVIS_PULL_REQUEST,
-    message = process.argv[2];
+    number = process.env.TRAVIS_PULL_REQUEST;
 
 var reporter = require('./index')(token, repoSlug, number);
 
